e97cc3fc31cda5cbe1642b97d6cde5c76671fdea
o-kinnen <o.kinnen@students.ephec.be>
2022-12-14 23:21:31 +0100

    Commit 1

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from expiration_date import ExpirationDate
+from foodProfile import FoodProfile
+from students import Student
+from fridge import Fridge
+from recipe import Recipe
 from food import Food
 import argparse
 
@@ -6,7 +11,7 @@ parser = argparse.ArgumentParser = argparse.ArgumentParser(
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    fridge = {}
+    fridge = Fridge({}, "", "", "", "", 0, {}, "")
     num = 1
     while True:
         print("1. Encoder son profil alimentaire\n2. Voir son frigo\n3. Ajouter des aliments\n4. Supprimer des "
@@ -15,25 +20,38 @@ if __name__ == "__main__":
         choice = input("Que voulez vous faire ? (taper le numéro) ")
 
         if choice == '1':
-            pass
+            lastname = input("Entrer votre nom: ")
+            firstname = input("Entrer votre prénom: ")
+            weight = int(input("Entrer votre poids: "))
+            height = int(input("Entrer votre taille: "))
+            age = int(input("Entrer votre âge: "))
+            gender = input("Entrer votre genre: ")
+            diet = input("Entrer votre diète: ")
+            allergy = input("Entrer vos éventuelles allergies: ")
+
+            food_profile = FoodProfile(firstname, lastname, diet, allergy, gender, age, weight, height)
+            print(food_profile)
+
         elif choice == '2':
-            pass
+            print(fridge.check_inside())
+
         elif choice == '3':
             response = "oui"
             while response.lower() == "oui":
-                fridge["aliment " + str(num)] = {}
                 libelle = input("Entrer le nom de l'aliment: ")
                 food_type = input("Entrer le type de l'aliment: ")
                 nutriscore = input("Entrer le nutriscore de l'aliment: ")
                 origin = input("Entrer l'origine de l'aliment: ")
                 caloric = int(input("Entrer le nombre de calorie de l'aliment: "))
-                nutritional_values = int(input("Entrer les valeurs nutritives de l'aliment: "))
+                nutritional_values = input("Entrer les valeurs nutritives de l'aliment: ")
                 expiration_date = input("Entrer la date de péremption (format AAAA-MM-DD): ")
                 food = Food(libelle, food_type, nutriscore, origin, caloric, nutritional_values, expiration_date)
+                fridge = Fridge({}, libelle, food_type, nutriscore, origin, caloric, nutritional_values, expiration_date)
+                fridge.add_food()
                 print(food)
-                num += 1
-                response = input("Voulez-vous encore insérer un aliment ? (si oui : taper oui/si non : taper sur une autre "
-                                 "touche) ")
+                response = input(
+                    "Voulez-vous encore insérer un aliment ? (si oui : taper oui/si non : taper sur une autre "
+                    "touche) ")
         elif choice == '4':
             pass
         elif choice == '5':
