49b37acf6dbe7b60500ed29df0612cc8962ee0d7
Arthur <arthurkalinowska@gmail.com>
2022-12-20 10:56:10 +0100

    Classe test des classes

---
+++ main.py
@@ -0,0 +1,9 @@
+from foodProfile import FoodProfile
+
+if __name__ == "__main__":
+    print("Test : classes students et foodProfile\n")
+    test1 = FoodProfile("Tuture", "LaFriture", "Omnivorous", "Aucune", "M", 19, 79, 180)
+    print(test1)
+
+    test2 = FoodProfile("Théa", "Mercier", "Omnivorous", "Aucune", "F", 19, 65, 171)
+    print(test2)
